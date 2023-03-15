b08d0ba50ad0b209a786f7f7b1d91710f36bf58b
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-03-14 18:30:55 -0600

    tratando de cambiar el color y no funciona

---
+++ ejemplo/cambiar.ps1
@@ -0,0 +1,3 @@
+﻿$archivoCss=".\css\estilos.css"
+$nuevoColor="#71fc88"
+(Get-Content -Path $archivoCss) -replace "#mi-id\s*{\s*color:\s*gold;","#mi-id { color: $nuevoColor;" | Set-Content -Path $archivoCss
--- holamundo.ps1
+++
@@ -1,2 +0,0 @@
-﻿Write-Host "Hola Mundo incierto!"
-Write-Host "Esto es una pinche pruebita desde el ISE"
